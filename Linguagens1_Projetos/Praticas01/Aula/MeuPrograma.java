fb48b3deadec0f440b35e5f2a2d38c11764663b0
FELIPE  DOS SANTOS PAGANINI <18.01226-4@maua.br>
2020-03-02 11:30:02 -0300

    Arquivo do Código

---
+++ Linguagens1_Projetos/Praticas01/Aula/MeuPrograma.java
@@ -0,0 +1,7 @@
+class MeuProrama{
+    public static void main(String[] args){
+        //Mensagem de saída do sistema
+        System.out.println("Olá Mundo!");
+        System.out.println("Estou programando no modo Conan. :");
+    }
+}

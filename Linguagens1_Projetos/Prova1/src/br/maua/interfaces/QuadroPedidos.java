39779335fc63d2e6efb8b72054f19fa253d791b4
FePaganini <felipespaganini@hotmail.com>
2020-06-23 00:08:11 -0300

    Finalizando Interface+JavaDocs QuadroPedidos

--- Linguagens1_Projetos/Prova1/src/br/maua/interfaces/QuadroPedidos.java
+++ Linguagens1_Projetos/Prova1/src/br/maua/interfaces/QuadroPedidos.java
@@ -1,4 +1,11 @@
 package br.maua.interfaces;
 
+/**
+ * Interface que mostrará os pedidos: Ids, formas de pagamento, descrições, valores, estados
+ * @author Felipe Dos Santos Paganini - felipespaganini@hotmail.com
+ * @since 22/06/2020
+ * @version 1.0
+ */
 public interface QuadroPedidos {
+    public void mostrarPedidos();
 }

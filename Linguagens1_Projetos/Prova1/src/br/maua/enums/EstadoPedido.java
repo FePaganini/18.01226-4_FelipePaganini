630797b2cb4fee0d80042536472460ec213899f1
FePaganini <felipespaganini@hotmail.com>
2020-06-22 23:35:10 -0300

    JavaDocs dos Enums

--- Linguagens1_Projetos/Prova1/src/br/maua/enums/EstadoPedido.java
+++ Linguagens1_Projetos/Prova1/src/br/maua/enums/EstadoPedido.java
@@ -1,5 +1,11 @@
 package br.maua.enums;
 
+/**
+ * Enum que apresenta o estado do pedido realizado na pizzaria
+ * @author Felipe Dos Santos Paganini - felipespaganini@hotmail.com
+ * @since 22/06/2020
+ * @version 1.0
+ */
 public enum EstadoPedido {
     REALIZADO, PREPARACAO, SAIU_PARA_ENTREGA,ENTREGUE,DEVOLVIDO;
 }
--- Linguagens1_Projetos/Prova1/src/br/maua/enums/FormaPagamento.java
+++ Linguagens1_Projetos/Prova1/src/br/maua/enums/FormaPagamento.java
@@ -1,5 +1,11 @@
 package br.maua.enums;
 
+/**
+ * Enum que apresenta as formas de pagamento a qual a pizzaria oferece
+ * @author Felipe Dos Santos Paganini - felipespaganini@hotmail.com
+ * @since 22/06/2020
+ * @version 1.0
+ */
 public enum FormaPagamento {
     DINHEIRO, DEBITO, CREDITO, VALE_ALIMENTACAO, VALE_REFEICAO;
 }

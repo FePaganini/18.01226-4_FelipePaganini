686f4b97c45d053d86d8aea1b43900998a5c3981
FePaganini <felipespaganini@hotmail.com>
2020-06-23 00:02:41 -0300

    Finalizando Interface+JavaDocs Autentificador

--- Linguagens1_Projetos/Prova1/src/br/maua/interfaces/Autentificador.java
+++ Linguagens1_Projetos/Prova1/src/br/maua/interfaces/Autentificador.java
@@ -1,4 +1,11 @@
 package br.maua.interfaces;
 
+/**
+ * Interface que verifica a senha do usuário para realizar novos pedidos ou alterar os já realizados
+ * @author Felipe Dos Santos Paganini - felipespaganini@hotmail.com
+ * @since 22/06/2020
+ * @version 1.0
+ */
 public interface Autentificador {
+    public void autentificacao();
 }

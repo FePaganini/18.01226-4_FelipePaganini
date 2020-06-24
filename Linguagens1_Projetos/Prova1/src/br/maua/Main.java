6c91ac652ea655dbd9cd4b88fa875e776fdd4dd6
FePaganini <felipespaganini@hotmail.com>
2020-06-24 00:20:35 -0300

    Atualizando JavaDocs(Finalizado)

--- Linguagens1_Projetos/Prova1/src/br/maua/Main.java
+++ Linguagens1_Projetos/Prova1/src/br/maua/Main.java
@@ -2,6 +2,12 @@ package br.maua;
 
 import br.maua.models.Pizzaria;
 
+/**
+ * Classe porta de entrada da aplicação base ela é responsável por iniciar a aplicação da classe Pizzaria
+ * @author Felipe Dos Santos Paganini - felipespaganini@hotmail.com
+ * @since 22/06/2020
+ * @version 1.0
+ */
 public class Main {
     public static void main(String[] args) {
         Pizzaria pizzaria = new Pizzaria();

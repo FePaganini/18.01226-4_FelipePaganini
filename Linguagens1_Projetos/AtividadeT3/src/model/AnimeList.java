dee0ae5cde608bb7ed6244d52067d737dee3f47a
FePaganini <felipespaganini@hotmail.com>
2020-10-04 01:18:12 -0300

    Criação das classes de lista de anime e manga

---
+++ Linguagens1_Projetos/AtividadeT3/src/model/AnimeList.java
@@ -0,0 +1,12 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AnimeList {
+    public List<Anime> animeList;
+
+    public  AnimeList(){
+        this.animeList = new ArrayList<>();
+    }
+}
